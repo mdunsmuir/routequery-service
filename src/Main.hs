cf484e7e415bfa99d381d277db1c28e0437960a3
Michael Dunsmuir <mjdunsmuir@gmail.com>
2015-03-07 18:38:19 -0800

    changed paths and added licenses

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,20 @@
+{-
+    Copyright (C) 2015  Michael Dunsmuir
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 import Control.Applicative
 import Data.Char (isSpace)
 import Data.Aeson
@@ -6,7 +23,7 @@ import Network.HTTP
 import Text.ProtocolBuffers.WireMessage
 import Com.Google.Transit.Realtime.FeedMessage (FeedMessage)
 
-import RoutequeryService.GTFS
+import RoutequeryService.GTFSRealtime
 
 main = do
   writeJsonForRequest "http://api.pugetsound.onebusaway.org/api/gtfs_realtime/trip-updates-for-agency/1.pb?key=" "trip_updates"
--- src/RoutequeryService/GTFSRealtime.hs
+++ src/RoutequeryService/GTFSRealtime.hs
@@ -1,6 +1,23 @@
+{-
+    Copyright (C) 2015  Michael Dunsmuir
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+-}
+
 {-# LANGUAGE ConstraintKinds, TemplateHaskell #-}
 
-module RoutequeryService.GTFS () where
+module RoutequeryService.GTFSRealtime () where
 
 import Control.Monad
 import qualified Data.Sequence as S
@@ -14,7 +31,7 @@ import Data.Aeson
 import Language.Haskell.TH
 import Data.Aeson.TH
 
-import RoutequeryService.GTFS.TH
+import RoutequeryService.GTFSRealtime.TH
 
 import Com.Google.Transit.Realtime.Alert
 import Com.Google.Transit.Realtime.Alert.Cause
